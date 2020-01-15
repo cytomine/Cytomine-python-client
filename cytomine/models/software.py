884cfdfd460a90b59150c648d32ca7a10d097b12
Ulysse Rubens <urubens@uliege.be>
2020-01-15 15:14:43 +0100

    Fix typo in job status update
    (cherry picked from commit 0eca29d2d037a7e0a3d335b14f0fad415e1ed1d1)

--- cytomine/models/software.py
+++ cytomine/models/software.py
@@ -212,7 +212,7 @@ class Job(Model):
                 job_id=self.id,
                 statusComment=attributes.get("statusComment", self.statusComment),
                 status=_HUMAN_READABLE_JOB_STATUS[attributes.get("status", self.status)],
-                progress=attributes.get("progess", self.progress)
+                progress=attributes.get("progress", self.progress)
             )
         )
         return super(Job, self).update(id=id, **attributes)
