096525da7850c3c1754a263ec3fb61d6456dcdd9
Ulysse Rubens <urubens@uliege.be>
2020-05-20 09:15:45 +0200

    Update to v2.6.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setup(
     name='Cytomine-Python-Client',
-    version='2.6.0',
+    version='2.6.1',
     description='Python client to interact with Cytomine.',
     long_description=long_description,
     long_description_content_type="text/markdown",
