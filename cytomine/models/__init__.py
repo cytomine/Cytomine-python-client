b974bd28f2323f3582fd3f0c86347f1be547f86e
Ulysse Rubens <urubens@uliege.be>
2021-02-01 13:14:01 +0100

    feat: Add models and collections for image group and annotation links
    (cherry picked from commit 601c2ea4b5298d0c9b6bd8754f83bb97bed185f4)

--- cytomine/models/__init__.py
+++ cytomine/models/__init__.py
@@ -20,12 +20,12 @@ __copyright__ = "Copyright 2010-2022 University of Liège, Belgium, http://www.c
 
 from .model import Model, DomainModel
 from .collection import Collection, DomainCollection
-from .annotation import Annotation, AnnotationCollection, AnnotationTerm, AlgoAnnotationTerm
+from .annotation import Annotation, AnnotationCollection, AnnotationTerm, AlgoAnnotationTerm, AnnotationGroup, \
+    AnnotationGroupCollection, AnnotationLink, AnnotationLinkCollection
 from .image import AbstractImage, AbstractImageCollection, AbstractSlice, AbstractSliceCollection,\
     ImageInstance, ImageInstanceCollection, SliceInstance, SliceInstanceCollection, \
     ImageServer, ImageServerCollection
-#from .imagegroup import ImageGroup, ImageGroupCollection, ImageGroupImageInstance, ImageGroupImageInstanceCollection
-#from .imagegroup import ImageGroup, ImageGroupCollection, ImageGroupHDF5, ImageSequence, ImageSequenceCollection
+from .imagegroup import ImageGroup, ImageGroupCollection, ImageGroupImageInstance, ImageGroupImageInstanceCollection
 from .ontology import Ontology, OntologyCollection, Term, TermCollection, RelationTerm
 from .project import Project, ProjectCollection, Discipline, DisciplineCollection
 from .property import Property, PropertyCollection, AttachedFile, AttachedFileCollection, Description, \
