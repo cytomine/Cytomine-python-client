15b8d7123d8a2ecff6e233f7341f043977925c8a
lrollus <loic.rollus@cytomine.com>
2021-06-07 11:03:06 +0200

    clean build version script

--- scripts/ciBuildVersion.sh
+++ scripts/ciBuildVersion.sh
@@ -7,20 +7,16 @@ srcRoot=$(git rev-parse --show-toplevel)
 cd $srcRoot
 
 # get version number from git
-gitLongTag=$(git describe --long --dirty --tags)
-branchName=${1:-$(git rev-parse --abbrev-ref HEAD)} #if no argument defined, get the branch name from git
+gitLongTag=$(git describe --long --dirty)
+# get the branch name from first arg or from git
+branchName=${1:-$(git rev-parse --abbrev-ref HEAD)}
 
-echo "BANCH NAME IS '$branchName'"
-#in the master branch, make sure the tag is clean ('1.2.3'; not 1.2.3-alpha) and there has been 0 commits since the tag has been set.
+# check if tag is an official release (v1.2.3) + no other commit behind (or dirty)
 if [[ $gitLongTag =~ v[0-9]+.[0-9]+.[0-9]+-0-[0-9a-g]{8,9}$ ]]; then
-
   versionNumber=$(echo $gitLongTag | sed -r "s/v([0-9]+\.[0-9]+\.[0-9]+)-[0-9]+-.+/\1/")
-
 else
-  echo "WARNING: invalid tag for a real release $gitLongTag"
-
-  versionNumber=0.0.0-$branchName-$(date "+%Y%m%d%H%M%S")-SNAPSHOT
-
+  echo "WARNING: invalid tag for an official release $gitLongTag"
+  versionNumber=$branchName-$(date "+%Y%m%d%H%M%S")-SNAPSHOT
 fi
 
 echo $versionNumber > ./ci/version
