096a27c78cc4c2411d362d3e4038bc4e3f300beb
Ulysse Rubens <urubens@uliege.be>
2022-05-20 17:41:41 +0200

    feat: add new get terms example

---
+++ examples/get_terms.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# * Copyright (c) 2009-2022. Authors: see NOTICE file.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+import sys
+from argparse import ArgumentParser
+
+
+from cytomine import Cytomine
+from cytomine.models import TermCollection
+
+
+# This example script allows you to get the list of terms (labels) in a project.
+
+logging.basicConfig()
+logger = logging.getLogger("cytomine.client")
+logger.setLevel(logging.INFO)
+
+if __name__ == '__main__':
+    parser = ArgumentParser(prog="Cytomine Python client example")
+
+    # Cytomine connection parameters
+    parser.add_argument('--cytomine_host', dest='host',
+                        default='demo.cytomine.be', help="The Cytomine host")
+    parser.add_argument('--cytomine_public_key', dest='public_key',
+                        help="The Cytomine public key")
+    parser.add_argument('--cytomine_private_key', dest='private_key',
+                        help="The Cytomine private key")
+
+    # Cytomine project ID
+    parser.add_argument('--cytomine_id_project', dest='id_project',
+                        help="The project from which we want the images")
+
+    params, other = parser.parse_known_args(sys.argv[1:])
+
+    with Cytomine(host=params.host, public_key=params.public_key, private_key=params.private_key) as cytomine:
+
+        terms = TermCollection().fetch_with_filter("project", params.id_project)
+        for term in terms:
+            print("Term ID: {} | Name: {} | Color: {}".format(term.id, term.name, term.color))
+
