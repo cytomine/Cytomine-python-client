95e318cecd2292b6eb63a43a282c691bf8597bff
waliens <romain.mormont@cytomine.com>
2022-11-09 17:12:35 +0100

    chore: update version 2.3.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ pillow_version_constraint = ',<7.0.0' if sys.version_info.major < 3 else ''
 
 setup(
     name='cytomine-python-client',
-    version='2.3.0',
+    version='2.3.3',
     description='Python client to interact with Cytomine.',
     long_description=long_description,
     long_description_content_type="text/markdown",
