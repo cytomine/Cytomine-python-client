610df437d0ad6fc6d9d98876e627f48575e8c79d
Ba Thien LE <bathien.le.pro@gmail.com>
2025-04-29 10:30:32 +0200

    docs: add 3.1.0 changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,32 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [3.1.0] - 2024-04-29
+
+### Added
+
+- AnnotationGroup model
+- AnnotationLink model
+- ImageGroup model
+- ImageGroupImageInstance model
+
+### Removed
+
+- Job model
+- ProcessingServer model
+- Software model
+- TrustedSource model
+- UserJob model
+- CytomineJob class
+
+## [3.0.0] - 2024-04-23
+
+[Unreleased]: https://github.com/cytomine/Cytomine-python-client/compare/3.1.0..HEAD
+[3.1.0]: https://github.com/cytomine/Cytomine-python-client/releases/tag/3.1.0
+[3.0.0]: https://github.com/cytomine/Cytomine-python-client/releases/tag/3.0.0
--- setup.py
+++ setup.py
@@ -13,42 +13,44 @@
 # * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # * See the License for the specific language governing permissions and
 # * limitations under the License.
-import sys
 
 from setuptools import setup
-from io import open
 
 with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
-
 setup(
-    name='cytomine-python-client',
-    version='x.x.x',
-    description='Python client to interact with Cytomine.',
+    name="cytomine-python-client",
+    version="3.1.0",
+    description="Python client to interact with Cytomine.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['cytomine', 'cytomine.models', 'cytomine.models._utilities', 'cytomine.utilities'],
-    url='https://www.cytomine.org',
-    classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Operating System :: OS Independent'
+    packages=[
+        "cytomine",
+        "cytomine.models",
+        "cytomine.models._utilities",
+        "cytomine.utilities",
     ],
-    install_requires=['requests-toolbelt>=0.8.0',
-                      'CacheControl>=0.12.10',
-                      'Shapely>=1.6.4',
-                      'six>=1.11.0',
-                      'requests>=2.27.1',
-                      'urllib3>=1.25.2'],
-    setup_requires=['pytest-runner'],
-    extra_requires={
-        "test": ['pytest']
-    },
-    test_suite='cytomine.tests',
-    license='LICENSE',
-    data_files=[('', ['LICENSE', 'NOTICE', 'requirements.txt'])]
+    url="https://uliege.cytomine.org",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests-toolbelt>=0.8.0",
+        "CacheControl>=0.12.10",
+        "Shapely>=1.6.4",
+        "six>=1.11.0",
+        "requests>=2.27.1",
+        "urllib3>=1.25.2",
+    ],
+    setup_requires=["pytest-runner"],
+    extra_requires={"test": ["pytest"]},
+    test_suite="cytomine.tests",
+    license="LICENSE",
+    data_files=[("", ["LICENSE", "NOTICE", "requirements.txt"])],
 )
